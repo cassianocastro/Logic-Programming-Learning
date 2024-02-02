1de3edfef1ecc1ad968e8f372303b5634d06c955
Cassiano Castro <development.castro@gmail.com>
2024-02-02 15:00:26 -0300

    Added classes and namespace test

---
+++ src/tests/Exercises.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <iomanip>
+#include <iostream>
+#include <stdexcept>
+
+namespace Tests
+{
+
+    /**
+     * List 01, Exercise 01.
+     */
+    class Exercise01
+    {
+
+        public:
+
+            /**
+             *
+             */
+            float getDolars(float, float);
+
+            /**
+             *
+             */
+            void doExercise(void);
+    };
+
+    /**
+     * List 01, Exercise 02.
+     */
+    class Exercise02
+    {
+
+        public:
+
+            /**
+             *
+             */
+            float celsiusToFarenheit(float);
+
+            /**
+             *
+             */
+            float celsiusToKelvin(float);
+
+            /**
+             *
+             */
+            void doExercise(void);
+    };
+
+}
