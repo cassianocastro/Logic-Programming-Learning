74d9d2015366ec8dbb21ba8c8b73f9f7d77a25d9
Cassiano Castro <development.castro@gmail.com>
2024-02-07 23:25:52 -0300

    Removed function docblocks

--- src/app/List_5.cpp
+++ src/app/List_5.cpp
@@ -5,11 +5,6 @@
 
 using std::cout, std::cin;
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
- * preenchidos aleatoriamente no intervalo 0 a 50, em seguida, escreva os números
- * gerados. O Tamanho do vetor deverá ser definido em uma variável constante.
- */
 void exercicio01(void)
 {
     const int ARRAY_SIZE = 10;
@@ -30,12 +25,6 @@ void exercicio01(void)
     cout << '\n';
 }
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
- * aleatoriamente no intervalo 0 a 50, em seguida, escreva os números gerados.
- * O Tamanho do vetor e o limite de geração dos número aleatórios deverá ser
- * definido em variável constante.
- */
 void exercicio02(void)
 {
     const unsigned short int ARRAY_SIZE = 10, RANDOM_LIMIT = 51;
@@ -70,12 +59,6 @@ int getRandomNumber(void)
     return number;
 }
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
- * pares preenchidos aleatoriamente no intervalo 0 a 50, em seguida,
- * escreva os números gerados. O Tamanho do vetor e o limite de geração dos
- * número aleatórios deverá ser definido em variável constante.
- */
 void exercicio03(void)
 {
     const unsigned int ARRAY_SIZE { 10 };
@@ -129,13 +112,6 @@ int getSmallestValueFromArray(const std::array<int, 10>* const array)
     return smallest;
 }
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
- * pares preenchidos aleatoriamente no intervalo 0 a 50, em seguida, escreva
- * o vetor gerado, o maior e o menor número existentes no vetor gerado. O Tamanho
- * do vetor e o limite de geração dos número aleatórios deverá ser definido em
- * variável constante.
- */
 void exercicio04(void)
 {
     const unsigned short int ARRAY_SIZE { 10 };
@@ -163,12 +139,6 @@ void exercicio04(void)
     ;
 }
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
- * preenchidos aleatoriamente no intervalo 20 a 50, em seguida, escreva os
- * números gerados. O Tamanho do vetor os limites inferior e superior de geração
- * dos número aleatórios deverá ser definido em variável constante.
- */
 void exercicio05(void)
 {
     const unsigned short int ARRAY_SIZE { 10 }, RANDOM_MIN { 20 }, RANDOM_LIMIT { 50 };
@@ -201,13 +171,6 @@ int even(const bool isEven)
     return ( number % 2 != 0 ) ? number : (number + 1);
 }
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
- * preenchidos aleatoriamente no intervalo 0 a 50, todavia, o vetor possui restrições.
- * As posições pares do vetor só admitem números pares e as posições impares do vetor
- * só admitem números impares. Escrever o vetor gerado. O Tamanho do vetor e o limite
- * de geração dos números aleatórios deverá ser definido em variável constante.
- */
 void exercicio06(void)
 {
     const unsigned short int ARRAY_SIZE { 10 };
@@ -229,14 +192,6 @@ void exercicio06(void)
     cout << '\n';
 }
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
- * aleatoriamente no intervalo 0 a 50, em seguida, separe os números pares e os números impares do
- * vetor gerador em dois vetores, um somente com os números pares encontrados no vetor gerado e
- * outro somente com os números impares encontrados no vetor gerado. Em seguida, escrever o vetor
- * gerado, o vetor somente de números pares e o vetor somente de números impares. O Tamanho do
- * vetor e o limite de geração dos número aleatórios deverá ser definido em variável constante.
- */
 void exercicio07(void)
 {
     const unsigned short int ARRAY_SIZE { 10 }, RANDOM_LIMIT { 51 };
@@ -279,12 +234,6 @@ void exercicio07(void)
     cout << '\n';
 }
 
-/**
- * Faça um algoritmo que gere um vetor de 5 posições preenchido com números aleatoriamente
- * gerados. Só serão admitidos no vetor números divisíveis por 3 no intervalo 0 a 100. Escrever o
- * vetor gerado. O Tamanho do vetor e o limite de geração dos número aleatórios deverá ser definido
- * em variável constante.
- */
 void exercicio08(void)
 {
     const unsigned short int ARRAY_SIZE { 5 }, RANDOM_LIMIT { 101 };
@@ -328,11 +277,6 @@ bool isPrime(const int dividend)
     return true;
 }
 
-/**
- * Faça um algoritmo que gere um vetor de 5 posições com números primos preenchidos
- * aleatoriamente no intervalo 0 a 100. O Tamanho do vetor e o limite de geração dos números
- * aleatórios deverá ser definido em variável constante.
- */
 void exercicio09(void)
 {
     const unsigned short int ARRAY_SIZE { 5 }, RANDOM_LIMIT { 101 };
@@ -358,12 +302,6 @@ void exercicio09(void)
     cout << '\n';
 }
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
- * aleatoriamente no intervalo 0 a 50. Os números não podem ser repetidos. Em seguida, escreva os
- * números gerados. O Tamanho do vetor e os limites inferior e superior de geração dos número
- * aleatórios deverá ser definido em variável constante.
- */
 void exercicio10(void)
 {
     const unsigned short int ARRAY_SIZE { 10 }, RANDOM_LIMIT { 51 };
