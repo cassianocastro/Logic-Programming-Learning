6b3113dab311bf6cca5576dd79542be0e0f9c8d5
Cassiano Castro <development.castro@gmail.com>
2024-02-08 17:48:24 -0300

    Include std headers and added function prototypes

--- src/includes/List_2.hpp
+++ src/includes/List_2.hpp
@@ -2,6 +2,62 @@
 
 #define LIST_2_HPP
 
+#include <array>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <string>
+#include <utility>
 
+/**
+ *
+ */
+void exercicio01(void);
+
+/**
+ *
+ */
+void exercicio02(void);
+
+/**
+ *
+ */
+void exercicio03(void);
+
+/**
+ *
+ */
+void exercicio04(void);
+
+/**
+ *
+ */
+void exercicio05(void);
+
+/**
+ *
+ */
+void exercicio06(void);
+
+/**
+ *
+ */
+void exercicio07(void);
+
+/**
+ *
+ */
+void exercicio08(void);
+
+/**
+ *
+ */
+void exercicio09(void);
+
+/**
+ *
+ */
+void exercicio10(void);
 
 #endif
--- src/includes/List_3.hpp
+++ src/includes/List_3.hpp
@@ -2,6 +2,59 @@
 
 #define LIST_3_HPP
 
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <map>
 
+/**
+ *
+ */
+void exercicio01(void);
+
+/**
+ *
+ */
+void exercicio02(void);
+
+/**
+ *
+ */
+void exercicio03(void);
+
+/**
+ *
+ */
+void exercicio04(void);
+
+/**
+ *
+ */
+void exercicio05(void);
+
+/**
+ *
+ */
+void exercicio06(void);
+
+/**
+ *
+ */
+void exercicio07(void);
+
+/**
+ *
+ */
+void exercicio08(void);
+
+/**
+ *
+ */
+void exercicio09(void);
+
+/**
+ *
+ */
+void exercicio10(void);
 
 #endif
