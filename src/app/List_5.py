93bd291b9369abca4b36ef14f03b04de88270fb6
Cassiano Castro <development.castro@gmail.com>
2024-02-07 23:17:28 -0300

    Rewrited code in C++

--- src/app/List_5.py
+++ src/app/List_5.py
@@ -1,180 +1,406 @@
-import random
+#include <array>
+#include <iostream>
+#include <iomanip>
+#include <vector>
 
-# NOTE: Faça um algoritmo que gere um vetor de 10 posições com números inteiros
-# NOTE: preenchidos aleatoriamente no intervalo 0 a 50, em seguida, escreva os números
-# NOTE: gerados. O Tamanho do vetor deverá ser definido em uma variável constante.
+using std::cout, std::cin;
 
-def exercicio01():
-    vector = []
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+ * preenchidos aleatoriamente no intervalo 0 a 50, em seguida, escreva os números
+ * gerados. O Tamanho do vetor deverá ser definido em uma variável constante.
+ */
+void exercicio01(void)
+{
+    const int ARRAY_SIZE = 10;
 
-    for i in range( 10 ):
-        vector.append(random.randint(1, 50))
-        print(vector[i])
+    std::array<int, ARRAY_SIZE> array {};
+
+    std::srand(time(nullptr));
+
+    cout << "Generated array: ";
+
+    for ( int i { 0 }; i < 10; ++i )
+    {
+        array.at(i) = rand() % 51;
+
+        cout << array.at(i) << ' ';
+    }
+
+    cout << '\n';
+}
+
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
+ * aleatoriamente no intervalo 0 a 50, em seguida, escreva os números gerados.
+ * O Tamanho do vetor e o limite de geração dos número aleatórios deverá ser
+ * definido em variável constante.
+ */
+void exercicio02(void)
+{
+    const unsigned short int ARRAY_SIZE = 10, RANDOM_LIMIT = 51;
+
+    std::array<int, ARRAY_SIZE> array {};
+
+    std::srand(time(nullptr));
+
+    cout << "Generated array: ";
+
+    for ( int i { 0 }; i < 10; ++i )
+    {
+        array.at(i) = rand() % RANDOM_LIMIT;
+
+        cout << array.at(i) << ' ';
+    }
+
+    cout << '\n';
+}
+
+int getRandomNumber(void)
+{
+    const unsigned short int RANDOM_LIMIT { 51 };
+
+    unsigned int number = rand() % RANDOM_LIMIT;
+
+    if ( number % 2 not_eq 0 )
+    {
+        ++number;
+    }
+
+    return number;
+}
+
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+ * pares preenchidos aleatoriamente no intervalo 0 a 50, em seguida,
+ * escreva os números gerados. O Tamanho do vetor e o limite de geração dos
+ * número aleatórios deverá ser definido em variável constante.
+ */
+void exercicio03(void)
+{
+    const unsigned int ARRAY_SIZE { 10 };
+
+    std::array<int, ARRAY_SIZE> array {};
+
+    std::srand(time(nullptr));
+
+    for ( int i { 0 }; i < ARRAY_SIZE; ++i )
+    {
+        array.at(i) = getRandomNumber();
+    }
+
+    cout << "Generated array: ";
+
+    for ( const auto& number : array )
+    {
+        cout << number << ' ';
+    }
+
+    cout << '\n';
+}
+
+int getBiggestValueFromArray(const std::array<int, 10>* const array)
+{
+    int biggest { 0 };
+
+    for ( const auto& number : *array )
+    {
+        if ( number > biggest )
+        {
+            biggest = number;
+        }
+    }
+
+    return biggest;
+}
+
+int getSmallestValueFromArray(const std::array<int, 10>* const array)
+{
+    int smallest { array->at(0) };
+
+    for ( const auto& number : *array )
+    {
+        if ( number < smallest )
+        {
+            smallest = number;
+        }
+    }
+
+    return smallest;
+}
+
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+ * pares preenchidos aleatoriamente no intervalo 0 a 50, em seguida, escreva
+ * o vetor gerado, o maior e o menor número existentes no vetor gerado. O Tamanho
+ * do vetor e o limite de geração dos número aleatórios deverá ser definido em
+ * variável constante.
+ */
+void exercicio04(void)
+{
+    const unsigned short int ARRAY_SIZE { 10 };
+
+    std::array<int, ARRAY_SIZE> array {};
+
+    std::srand(time(nullptr));
+
+    for ( int i { 0 }; i < ARRAY_SIZE; ++i )
+    {
+        array.at(i) = getRandomNumber();
+    }
+
+    cout << "Generated array: ";
+
+    for ( const auto& number : array )
+    {
+        cout << number << ' ';
+    }
+
+    cout
+        << "\nMaior número: " << getBiggestValueFromArray(&array)
+        << "\nMenor número: " << getSmallestValueFromArray(&array)
+        << '\n'
+    ;
+}
+
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+ * preenchidos aleatoriamente no intervalo 20 a 50, em seguida, escreva os
+ * números gerados. O Tamanho do vetor os limites inferior e superior de geração
+ * dos número aleatórios deverá ser definido em variável constante.
+ */
+void exercicio05(void)
+{
+    const unsigned short int ARRAY_SIZE { 10 }, RANDOM_MIN { 20 }, RANDOM_LIMIT { 50 };
+
+    std::array<int, ARRAY_SIZE> vector {};
+
+    std::srand(time(nullptr));
+
+    for ( int i { 0 }; i < ARRAY_SIZE; ++i )
+    {
+        vector.at(i) = RANDOM_MIN + rand() % (RANDOM_LIMIT - RANDOM_MIN);
+
+        cout << vector.at(i) << ' ';
+    }
+
+    cout << '\n';
+}
+
+int even(const bool isEven)
+{
+    const unsigned short int RANDOM_LIMIT { 51 };
+
+    int number = rand() % RANDOM_LIMIT;
+
+    if ( isEven )
+    {
+        return ( number % 2 == 0 ) ? number : (number + 1);
+    }
+
+    return ( number % 2 != 0 ) ? number : (number + 1);
+}
+
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+ * preenchidos aleatoriamente no intervalo 0 a 50, todavia, o vetor possui restrições.
+ * As posições pares do vetor só admitem números pares e as posições impares do vetor
+ * só admitem números impares. Escrever o vetor gerado. O Tamanho do vetor e o limite
+ * de geração dos números aleatórios deverá ser definido em variável constante.
+ */
+void exercicio06(void)
+{
+    const unsigned short int ARRAY_SIZE { 10 };
+
+    std::array<int, ARRAY_SIZE> array {};
+
+    std::srand((unsigned) time(nullptr));
+
+    for ( int i { 0 }; i < ARRAY_SIZE; ++i )
+    {
+        array.at(i) = even(i % 2 == 0);
+    }
+
+    for ( const auto& number : array )
+    {
+        cout << number << ' ';
+    }
+
+    cout << '\n';
+}
+
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
+ * aleatoriamente no intervalo 0 a 50, em seguida, separe os números pares e os números impares do
+ * vetor gerador em dois vetores, um somente com os números pares encontrados no vetor gerado e
+ * outro somente com os números impares encontrados no vetor gerado. Em seguida, escrever o vetor
+ * gerado, o vetor somente de números pares e o vetor somente de números impares. O Tamanho do
+ * vetor e o limite de geração dos número aleatórios deverá ser definido em variável constante.
+ */
+void exercicio07(void)
+{
+    const unsigned short int ARRAY_SIZE { 10 }, RANDOM_LIMIT { 51 };
+
+    std::array<int, ARRAY_SIZE> array {};
+
+    std::vector<int> odd {}, even {};
+
+    for ( int i { 0 }; i < ARRAY_SIZE; ++i )
+    {
+        array[i] = rand() % RANDOM_LIMIT;
+
+        if ( array[i] % 2 == 0 )
+            even.push_back(array[i]);
+        else
+            odd.push_back(array[i]);
+    }
+
+    cout << "Generated array:\n";
+
+    for ( const auto& number : array )
+    {
+        cout << number << ' ';
+    }
+
+    cout << "\nEven numbers vector:\n";
+
+    for ( const auto& number : even )
+    {
+        cout << number << ' ';
+    }
+
+    cout << "\nOdd numbers vector:\n";
+
+    for ( const auto& number : odd )
+    {
+        cout << number << ' ';
+    }
+
+    cout << '\n';
+}
+
+/**
+ * Faça um algoritmo que gere um vetor de 5 posições preenchido com números aleatoriamente
+ * gerados. Só serão admitidos no vetor números divisíveis por 3 no intervalo 0 a 100. Escrever o
+ * vetor gerado. O Tamanho do vetor e o limite de geração dos número aleatórios deverá ser definido
+ * em variável constante.
+ */
+void exercicio08(void)
+{
+    const unsigned short int ARRAY_SIZE { 5 }, RANDOM_LIMIT { 101 };
+
+    std::array<int, ARRAY_SIZE> array {};
+
+    std::srand((unsigned int) time(nullptr));
+
+    for ( int i { 0 }, n { 0 }; i < ARRAY_SIZE; ++i )
+    {
+        do n = rand() % RANDOM_LIMIT; while ( n % 3 != 0 );
+
+        array.at(i) = n;
+    }
+
+    cout << "Generated array: ";
+
+    for ( const auto& number : array )
+    {
+        cout << number << ' ';
+    }
+
+    cout << '\n';
+}
+
+bool isPrime(const int dividend)
+{
+    if ( dividend <= 1 )
+    {
+        return false;
+    }
+
+    for ( int divisor { 2 }; divisor < dividend; ++divisor )
+    {
+        if ( dividend % divisor == 0 )
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/**
+ * Faça um algoritmo que gere um vetor de 5 posições com números primos preenchidos
+ * aleatoriamente no intervalo 0 a 100. O Tamanho do vetor e o limite de geração dos números
+ * aleatórios deverá ser definido em variável constante.
+ */
+void exercicio09(void)
+{
+    const unsigned short int ARRAY_SIZE { 5 }, RANDOM_LIMIT { 101 };
+
+    std::array<int, 5> array {};
+
+    std::srand((unsigned int) time(nullptr));
+
+    for ( int i { 0 }, n { 0 }; i < ARRAY_SIZE; ++i )
+    {
+        do n = rand() % RANDOM_LIMIT; while ( not isPrime(n) );
+
+        array.at(i) = n;
+    }
+
+    cout << "Generated array: ";
+
+    for ( const auto& n : array )
+    {
+        cout << n << ' ';
+    }
+
+    cout << '\n';
+}
+
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
+ * aleatoriamente no intervalo 0 a 50. Os números não podem ser repetidos. Em seguida, escreva os
+ * números gerados. O Tamanho do vetor e os limites inferior e superior de geração dos número
+ * aleatórios deverá ser definido em variável constante.
+ */
+void exercicio10(void)
+{
+    const unsigned short int ARRAY_SIZE { 10 }, RANDOM_LIMIT { 51 };
+
+    std::array<int, ARRAY_SIZE> array {};
+
+    std::srand((unsigned int) time(nullptr));
+
+    for ( int i { 0 }, n { 0 }; i < ARRAY_SIZE; ++i )
+    {
+        n = rand() % RANDOM_LIMIT;
+
+        for ( int j = 0; j < i; ++j )
+        {
+            if ( n == array[j] )
+            {
+                i = 0;
+            }
+        }
+
+        array[i] = n;
+    }
+
+    cout << "Generated array: ";
+
+    for ( const auto& n : array )
+    {
+        cout << n << ' ';
+    }
+
+    cout << '\n';
+}
 
 
-# NOTE: Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
-# NOTE: aleatoriamente no intervalo 0 a 50, em seguida, escreva os números gerados.
-# NOTE: O Tamanho do vetor e o limite de geração dos número aleatórios deverá ser
-# NOTE: definido em variável constante.
+int main(void)
+{
+    exercicio10();
 
-def exercicio02():
-    exercicio01()
-
-# NOTE: Faça um algoritmo que gere um vetor de 10 posições com números inteiros
-# NOTE: pares preenchidos aleatoriamente no intervalo 0 a 50, em seguida,
-# NOTE: escreva os números gerados. O Tamanho do vetor e o limite de geração dos
-# NOTE: número aleatórios deverá ser definido em variável constante.
-def getRandomNumber():
-    randomNumber = random.randint(1, 50)
-    if randomNumber % 2 != 0:
-        randomNumber += 1
-    return randomNumber
-
-def exercicio03():
-    vector = []
-
-    for i in range( 10 ):
-        vector.append(getRandomNumber())
-
-    print(vector)
-
-# NOTE: Faça um algoritmo que gere um vetor de 10 posições com números inteiros
-# NOTE: pares preenchidos aleatoriamente no intervalo 0 a 50, em seguida, escreva
-# NOTE: o vetor gerado, o maior e o menor número existentes no vetor gerado. O Tamanho
-# NOTE: do vetor e o limite de geração dos número aleatórios deverá ser definido em
-# NOTE: variável constante.
-
-def exercicio04():
-    vector = []
-
-    for i in range( 10 ):
-        vector.append(getRandomNumber())
-    print(
-        "{}\
-        \nMaior número encontrado: {}\
-        \nMenor número encontrado: {}"
-        .format(vector, max(vector), min(vector))
-    )
-# NOTE: Faça um algoritmo que gere um vetor de 10 posições com números inteiros
-# NOTE: preenchidos aleatoriamente no intervalo 20 a 50, em seguida, escreva os
-# NOTE: números gerados. O Tamanho do vetor os limites inferior e superior de geração
-# NOTE: dos número aleatórios deverá ser definido em variável constante.
-
-def exercicio05():
-    vector = []
-
-    for i in range( 10 ):
-        vector.append(random.randint(20, 50))
-        print(vector[i])
-
-# NOTE: Faça um algoritmo que gere um vetor de 10 posições com números inteiros
-# NOTE: preenchidos aleatoriamente no intervalo 0 a 50, todavia, o vetor possui restrições.
-# NOTE: As posições pares do vetor só admitem números pares e as posições impares do vetor
-# NOTE: só admitem números impares. Escrever o vetor gerado. O Tamanho do vetor e o limite
-# NOTE: de geração dos número aleatórios deverá ser definido em variável constante.
-
-def exercicio06():
-    def even(isEven):
-        randomNumber = random.randint(0, 50)
-        if isEven:
-            if randomNumber % 2 == 0:
-                return randomNumber
-            else:
-                return randomNumber + 1
-        else:
-            if randomNumber % 2 != 0:
-                return randomNumber
-            else:
-                return randomNumber + 1
-    vector = []
-
-    for i in range( 10 ):
-        if i % 2 == 0:
-            number = even(True)
-        else:
-            number = even(False)
-        vector.append(number)
-    print(vector)
-
-# NOTE: Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
-# NOTE: aleatoriamente no intervalo 0 a 50, em seguida, separe os números pares e os números impares do
-# NOTE: vetor gerador em dois vetores, um somente com os números pares encontrados no vetor gerado e
-# NOTE: outro somente com os números impares encontrados no vetor gerado. Em seguida, escrever o vetor
-# NOTE: gerado, o vetor somente de números pares e o vetor somente de números impares. O Tamanho do
-# NOTE: vetor e o limite de geração dos número aleatórios deverá ser definido em variável constante.
-
-def exercicio07():
-    vectorOdd, vectorEven, vector = [], [], [None] * 10
-
-    for i in range( 10 ):
-        vector[i] = random.randint(0, 50)
-
-        if vector[i] % 2 == 0:
-            vectorEven.append(vector[i])
-        else:
-            vectorOdd.append(vector[i])
-    print(
-        "{}\n"
-        "Vetor de Números Pares:\n"
-        "{}\n"
-        "Vetor de Números Ímpares:\n"
-        "{}\n".format(vector, vectorEven, vectorOdd)
-    )
-
-# NOTE: Faça um algoritmo que gere um vetor de 5 posições preenchido com números aleatoriamente
-# NOTE: gerados. Só serão admitidos no vetor números divisíveis por 3 no intervalo 0 a 100. Escrever o
-# NOTE: vetor gerado. O Tamanho do vetor e o limite de geração dos número aleatórios deverá ser definido
-# NOTE: em variável constante.
-
-def exercicio08():
-    vector = [0] * 5
-
-    for i in range( 5 ):
-        randomNumber = random.randint(0, 100)
-        while randomNumber % 3 != 0:
-            randomNumber = random.randint(0, 100)
-        vector[i] = randomNumber
-    print(vector)
-
-# NOTE: Faça um algoritmo que gere um vetor de 5 posições com números primos preenchidos
-# NOTE: aleatoriamente no intervalo 0 a 100. O Tamanho do vetor e o limite de geração dos números
-# NOTE: aleatórios deverá ser definido em variável constante.
-
-def exercicio09():
-    def isPrime(dividendo):
-        if dividendo > 1:
-            for divisor in range(2, dividendo):
-                if dividendo % divisor == 0:
-                    return False
-        else:
-            return False
-        return True
-
-    vector = [0] * 5
-
-    for i in range( 5 ):
-        randomNumber = random.randint(0, 100)
-
-        while not isPrime(randomNumber):
-            randomNumber = random.randint(0, 100)
-        vector[i] = randomNumber
-
-    print(vector)
-
-# NOTE: Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
-# NOTE: aleatoriamente no intervalo 0 a 50. Os números não podem ser repetidos. Em seguida, escreva os
-# NOTE: números gerados. O Tamanho do vetor e os limites inferior e superior de geração dos número
-# NOTE: aleatórios deverá ser definido em variável constante.
-
-def exercicio10():
-    vector = [0] * 5
-
-    for i in range(0, 5):
-        randomNumber = random.randint(0, 5)
-
-
-
-    print(vector)
-
-if __name__ == '__main__':
-    exercicio09()
+    return 0;
+}
