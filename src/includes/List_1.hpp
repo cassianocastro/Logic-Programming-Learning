c0ec5dbeccd850811f01de18590ce9e335e9650b
Cassiano Castro <development.castro@gmail.com>
2024-02-08 16:13:43 -0300

    Removed unnecessary enum

--- src/includes/List_1.hpp
+++ src/includes/List_1.hpp
@@ -9,24 +9,6 @@
 typedef unsigned char Byte;
 
 /**
- *
- */
-typedef enum
-{
-    SAIR,
-    EXERCICIO1,
-    EXERCICIO2,
-    EXERCICIO3,
-    EXERCICIO4,
-    EXERCICIO5,
-    EXERCICIO6,
-    EXERCICIO7,
-    EXERCICIO8,
-    EXERCICIO9,
-    EXERCICIO10
-} Opcoes;
-
-/**
  * Faça um programa que converta a moeda real em dólar,
  * informando-se apenas a cotação do dólar e o valor em reais.
  */
