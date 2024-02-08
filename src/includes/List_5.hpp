d421660d0da3e333fc3dd9d7e32cba314d9a23e2
Cassiano Castro <development.castro@gmail.com>
2024-02-07 23:25:28 -0300

    Added docblocks to prototypes

--- src/includes/List_5.hpp
+++ src/includes/List_5.hpp
@@ -2,24 +2,86 @@
 
 #define LIST_5_HPP
 
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+ * preenchidos aleatoriamente no intervalo 0 a 50, em seguida, escreva os números
+ * gerados. O Tamanho do vetor deverá ser definido em uma variável constante.
+ */
 void exercicio01(void);
 
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
+ * aleatoriamente no intervalo 0 a 50, em seguida, escreva os números gerados.
+ * O Tamanho do vetor e o limite de geração dos número aleatórios deverá ser
+ * definido em variável constante.
+ */
 void exercicio02(void);
 
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+ * pares preenchidos aleatoriamente no intervalo 0 a 50, em seguida,
+ * escreva os números gerados. O Tamanho do vetor e o limite de geração dos
+ * número aleatórios deverá ser definido em variável constante.
+ */
 void exercicio03(void);
 
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+ * pares preenchidos aleatoriamente no intervalo 0 a 50, em seguida, escreva
+ * o vetor gerado, o maior e o menor número existentes no vetor gerado. O Tamanho
+ * do vetor e o limite de geração dos número aleatórios deverá ser definido em
+ * variável constante.
+ */
 void exercicio04(void);
 
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+ * preenchidos aleatoriamente no intervalo 20 a 50, em seguida, escreva os
+ * números gerados. O Tamanho do vetor os limites inferior e superior de geração
+ * dos número aleatórios deverá ser definido em variável constante.
+ */
 void exercicio05(void);
 
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+ * preenchidos aleatoriamente no intervalo 0 a 50, todavia, o vetor possui restrições.
+ * As posições pares do vetor só admitem números pares e as posições impares do vetor
+ * só admitem números impares. Escrever o vetor gerado. O Tamanho do vetor e o limite
+ * de geração dos números aleatórios deverá ser definido em variável constante.
+ */
 void exercicio06(void);
 
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
+ * aleatoriamente no intervalo 0 a 50, em seguida, separe os números pares e os números impares do
+ * vetor gerador em dois vetores, um somente com os números pares encontrados no vetor gerado e
+ * outro somente com os números impares encontrados no vetor gerado. Em seguida, escrever o vetor
+ * gerado, o vetor somente de números pares e o vetor somente de números impares. O Tamanho do
+ * vetor e o limite de geração dos número aleatórios deverá ser definido em variável constante.
+ */
 void exercicio07(void);
 
+/**
+ * Faça um algoritmo que gere um vetor de 5 posições preenchido com números aleatoriamente
+ * gerados. Só serão admitidos no vetor números divisíveis por 3 no intervalo 0 a 100. Escrever o
+ * vetor gerado. O Tamanho do vetor e o limite de geração dos número aleatórios deverá ser definido
+ * em variável constante.
+ */
 void exercicio08(void);
 
+/**
+ * Faça um algoritmo que gere um vetor de 5 posições com números primos preenchidos
+ * aleatoriamente no intervalo 0 a 100. O Tamanho do vetor e o limite de geração dos números
+ * aleatórios deverá ser definido em variável constante.
+ */
 void exercicio09(void);
 
+/**
+ * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
+ * aleatoriamente no intervalo 0 a 50. Os números não podem ser repetidos. Em seguida, escreva os
+ * números gerados. O Tamanho do vetor e os limites inferior e superior de geração dos número
+ * aleatórios deverá ser definido em variável constante.
+ */
 void exercicio10(void);
 
 #endif
