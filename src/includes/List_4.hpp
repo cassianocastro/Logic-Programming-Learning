8f6d2ab9ce87a343055cc9867e6f29e2f3eeda8c
Cassiano Castro <development.castro@gmail.com>
2024-02-07 23:32:56 -0300

    Added function prototypes

--- src/includes/List_4.hpp
+++ src/includes/List_4.hpp
@@ -2,6 +2,24 @@
 
 #define LIST_4_HPP
 
+void exercicio01(void);
 
+void exercicio02(void);
+
+void exercicio03(void);
+
+void exercicio04(void);
+
+void exercicio05(void);
+
+void exercicio06(void);
+
+void exercicio07(void);
+
+void exercicio08(void);
+
+void exercicio09(void);
+
+void exercicio10(void);
 
 #endif
