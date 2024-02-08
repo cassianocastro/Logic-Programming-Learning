5f154f152fbc978100a2b1eb3ade113f544c38ea
Cassiano Castro <development.castro@gmail.com>
2024-02-07 23:36:23 -0300

    Added docblocks to prototypes

--- src/includes/List_4.hpp
+++ src/includes/List_4.hpp
@@ -2,24 +2,63 @@
 
 #define LIST_4_HPP
 
+/**
+ * Faça um algoritmo que leia um número, e seguida,
+ * informe se o número digitado é divisível por 3 e por 5.
+ */
 void exercicio01(void);
 
+/**
+ * Faça um algoritmo que solicite um numero, em seguida,
+ * mostre quantos e quais são seus divisores.
+ */
 void exercicio02(void);
 
+/**
+ * Faça um algoritmo que leia um vetor de 10 posições com
+ * números inteiros, em seguida, escreva
+ * os números na ordem inversa que foram digitados.
+ */
 void exercicio03(void);
 
+/**
+ * Faça um algoritmo que leia um vetor de 10 posições com números inteiros,
+ * em seguida, mostre a soma e a média dos números digitados.
+ */
 void exercicio04(void);
 
+/**
+ * Faça um algoritmo que leia um vetor de 10 números. Em seguida,
+ * escreva na tela o resultado de cada posição do vetor multiplicado por 2.
+ */
 void exercicio05(void);
 
+/**
+ * Faça um algoritmo que leia um vetor de 10 posições.
+ * Em seguida, informe quais números do vetor digitado são divisíveis por 3 e 5.
+ */
 void exercicio06(void);
 
+/**
+ * Faça um algoritmo que leia um vetor de 10 posições.
+ * Em seguida, informe quais números do vetor digitado são pares,
+ * quais são impares e quais são primos
+ */
 void exercicio07(void);
 
+/**
+ *
+ */
 void exercicio08(void);
 
+/**
+ *
+ */
 void exercicio09(void);
 
+/**
+ *
+ */
 void exercicio10(void);
 
 #endif
