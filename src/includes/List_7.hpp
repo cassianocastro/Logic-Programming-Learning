6d43d8568d3b90edbd7248cf3de70000bb1a15cb
Cassiano Castro <development.castro@gmail.com>
2024-02-09 15:15:21 -0300

    Added namespace and function prototypes

--- src/includes/List_7.hpp
+++ src/includes/List_7.hpp
@@ -2,6 +2,58 @@
 
 #define LIST_7_HPP
 
+namespace List_7
+{
 
+    /**
+     *
+     */
+    void exercise01(void);
+
+    /**
+     *
+     */
+    void exercise02(void);
+
+    /**
+     *
+     */
+    void exercise03(void);
+
+    /**
+     *
+     */
+    void exercise04(void);
+
+    /**
+     *
+     */
+    void exercise05(void);
+
+    /**
+     *
+     */
+    void exercise06(void);
+
+    /**
+     *
+     */
+    void exercise07(void);
+
+    /**
+     *
+     */
+    void exercise08(void);
+
+    /**
+     *
+     */
+    void exercise09(void);
+
+    /**
+     *
+     */
+    void exercise10(void);
+}
 
 #endif
