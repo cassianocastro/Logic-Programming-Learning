a548b22eda6145d994b30f7a485b9a63d9aa4376
Cassiano Castro <development.castro@gmail.com>
2024-02-09 14:59:19 -0300

    Namespace addition

--- src/includes/List_4.hpp
+++ src/includes/List_4.hpp
@@ -7,78 +7,82 @@
 #include <iomanip>
 #include <vector>
 
-/**
- * Faça um algoritmo que leia um número, e em seguida,
- * informe se o número digitado é divisível por 3 e por 5.
- */
-void exercicio01(void);
+namespace List_4
+{
 
-/**
- * Faça um algoritmo que solicite um número, e em seguida,
- * mostre quantos e quais são seus divisores.
- */
-void exercicio02(void);
+    /**
+     * Faça um algoritmo que leia um número, e em seguida,
+     * informe se o número digitado é divisível por 3 e por 5.
+     */
+    void exercicio01(void);
 
-/**
- * Faça um algoritmo que leia um vetor de 10 posições com números inteiros,
- * em seguida, escreva os números na ordem inversa que foram digitados.
- */
-void exercicio03(void);
+    /**
+     * Faça um algoritmo que solicite um número, e em seguida,
+     * mostre quantos e quais são seus divisores.
+     */
+    void exercicio02(void);
 
-/**
- * Faça um algoritmo que leia um vetor de 10 posições com números inteiros,
- * em seguida, mostre a soma e a média dos números digitados.
- */
-void exercicio04(void);
+    /**
+     * Faça um algoritmo que leia um vetor de 10 posições com números inteiros,
+     * em seguida, escreva os números na ordem inversa que foram digitados.
+     */
+    void exercicio03(void);
 
-/**
- * Faça um algoritmo que leia um vetor de 10 números. Em seguida,
- * escreva na tela o resultado de cada posição do vetor multiplicado por 2.
- */
-void exercicio05(void);
+    /**
+     * Faça um algoritmo que leia um vetor de 10 posições com números inteiros,
+     * em seguida, mostre a soma e a média dos números digitados.
+     */
+    void exercicio04(void);
 
-/**
- * Faça um algoritmo que leia um vetor de 10 posições.
- * Em seguida, informe quais números do vetor são divisíveis por 3 e por 5.
- */
-void exercicio06(void);
+    /**
+     * Faça um algoritmo que leia um vetor de 10 números. Em seguida,
+     * escreva na tela o resultado de cada posição do vetor multiplicado por 2.
+     */
+    void exercicio05(void);
 
-/**
- * Faça um algoritmo que leia um vetor de 10 posições. Em seguida,
- * informe quais números do vetor são pares, quais são impares e quais são
- * primos.
- */
-void exercicio07(void);
+    /**
+     * Faça um algoritmo que leia um vetor de 10 posições.
+     * Em seguida, informe quais números do vetor são divisíveis por 3 e por 5.
+     */
+    void exercicio06(void);
 
-/**
- * Faça um algoritmo que leia um vetor de 10 posições.
- * Em seguida, um segundo vetor recebe os números do primeiro vetor na ordem inversa.
- *
- * Escrever o vetor inicial e o vetor resultante:
- *
- * Exemplo:
- * Vetor inicial:    7 5 3 1 8 6 4 2 9 0
- * Vetor resultante: 0 9 2 4 6 8 1 3 5 7
- */
-void exercicio08(void);
+    /**
+     * Faça um algoritmo que leia um vetor de 10 posições. Em seguida,
+     * informe quais números do vetor são pares, quais são impares e quais são
+     * primos.
+     */
+    void exercicio07(void);
 
-/**
- * Faça um algoritmo que leia 2 vetores de 5 posições com números inteiros,
- * em seguida, armazene em um terceiro vetor a soma do primeiro e do segundo
- * vetor em suas respectivas posições.
- *
- * Exemplo:
- * Vetor 1: 3 4 5 1 7
- * Vetor 2: 1 2 3 8 0
- * Vetor 3: 4 6 8 9 7 <---- Soma
- */
-void exercicio09(void);
+    /**
+     * Faça um algoritmo que leia um vetor de 10 posições.
+     * Em seguida, um segundo vetor recebe os números do primeiro vetor na ordem inversa.
+     *
+     * Escrever o vetor inicial e o vetor resultante:
+     *
+     * Exemplo:
+     * Vetor inicial:    7 5 3 1 8 6 4 2 9 0
+     * Vetor resultante: 0 9 2 4 6 8 1 3 5 7
+     */
+    void exercicio08(void);
 
-/**
- * Faça um algoritmo que leia um vetor de 10 posições.
- *
- * Em seguida, informe qual o maior número existente no vetor.
- */
-void exercicio10(void);
+    /**
+     * Faça um algoritmo que leia 2 vetores de 5 posições com números inteiros,
+     * em seguida, armazene em um terceiro vetor a soma do primeiro e do segundo
+     * vetor em suas respectivas posições.
+     *
+     * Exemplo:
+     * Vetor 1: 3 4 5 1 7
+     * Vetor 2: 1 2 3 8 0
+     * Vetor 3: 4 6 8 9 7 <---- Soma
+     */
+    void exercicio09(void);
+
+    /**
+     * Faça um algoritmo que leia um vetor de 10 posições.
+     *
+     * Em seguida, informe qual o maior número existente no vetor.
+     */
+    void exercicio10(void);
+}
 
 #endif
--- src/includes/List_5.hpp
+++ src/includes/List_5.hpp
@@ -7,110 +7,114 @@
 #include <iomanip>
 #include <vector>
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
- * preenchidos aleatoriamente no intervalo 0 a 50, em seguida, escreva os números
- * gerados.
- *
- * O tamanho do vetor deverá ser definido em uma variável constante.
- */
-void exercicio01(void);
+namespace List_5
+{
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
- * aleatoriamente no intervalo de 0 a 50, em seguida, escreva os números gerados.
- *
- * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
- * definidos em variáveis constantes.
- */
-void exercicio02(void);
+    /**
+     * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+     * preenchidos aleatoriamente no intervalo 0 a 50, em seguida, escreva os números
+     * gerados.
+     *
+     * O tamanho do vetor deverá ser definido em uma variável constante.
+     */
+    void exercicio01(void);
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
- * pares preenchidos aleatoriamente no intervalo de 0 a 50, em seguida,
- * escreva os números gerados.
- *
- * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
- * definidos em variáveis constantes.
- */
-void exercicio03(void);
+    /**
+     * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
+     * aleatoriamente no intervalo de 0 a 50, em seguida, escreva os números gerados.
+     *
+     * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
+     * definidos em variáveis constantes.
+     */
+    void exercicio02(void);
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
- * pares preenchidos aleatoriamente no intervalo de 0 a 50, em seguida, escreva
- * o vetor gerado, o maior e o menor número existentes no vetor.
- *
- * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
- * definidos em variáveis constantes.
- */
-void exercicio04(void);
+    /**
+     * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+     * pares preenchidos aleatoriamente no intervalo de 0 a 50, em seguida,
+     * escreva os números gerados.
+     *
+     * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
+     * definidos em variáveis constantes.
+     */
+    void exercicio03(void);
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
- * preenchidos aleatoriamente no intervalo de 20 a 50, em seguida, escreva os
- * números gerados.
- *
- * O tamanho do vetor os limites inferior e superior de geração
- * dos números aleatórios deverão ser definidos em variáveis constantes.
- */
-void exercicio05(void);
+    /**
+     * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+     * pares preenchidos aleatoriamente no intervalo de 0 a 50, em seguida, escreva
+     * o vetor gerado, o maior e o menor número existentes no vetor.
+     *
+     * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
+     * definidos em variáveis constantes.
+     */
+    void exercicio04(void);
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
- * preenchidos aleatoriamente no intervalo de 0 a 50, todavia,
- * o vetor possui restrições. As posições pares do vetor só admitem números pares e
- * as posições impares do vetor só admitem números impares.
- *
- * Escrever o vetor gerado.
- *
- * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
- * definidos em variáveis constantes.
- */
-void exercicio06(void);
+    /**
+     * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+     * preenchidos aleatoriamente no intervalo de 20 a 50, em seguida, escreva os
+     * números gerados.
+     *
+     * O tamanho do vetor os limites inferior e superior de geração
+     * dos números aleatórios deverão ser definidos em variáveis constantes.
+     */
+    void exercicio05(void);
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
- * aleatoriamente no intervalo de 0 a 50, em seguida, separe os números pares e os números impares do
- * vetor gerador em dois vetores, um somente com os números pares encontrados no vetor gerado e
- * outro somente com os números impares encontrados no vetor gerado.
- *
- * Em seguida, escrever o vetor gerado, o vetor somente de números pares e
- * o vetor somente de números impares.
- *
- * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
- * definidos em variáveis constantes.
- */
-void exercicio07(void);
+    /**
+     * Faça um algoritmo que gere um vetor de 10 posições com números inteiros
+     * preenchidos aleatoriamente no intervalo de 0 a 50, todavia,
+     * o vetor possui restrições. As posições pares do vetor só admitem números pares e
+     * as posições impares do vetor só admitem números impares.
+     *
+     * Escrever o vetor gerado.
+     *
+     * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
+     * definidos em variáveis constantes.
+     */
+    void exercicio06(void);
 
-/**
- * Faça um algoritmo que gere um vetor de 5 posições preenchido com números aleatoriamente
- * gerados. Só serão admitidos no vetor números divisíveis por 3 no intervalo de 0 a 100.
- *
- * Escrever o vetor gerado.
- *
- * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
- * definidos em variáveis constantes.
- */
-void exercicio08(void);
+    /**
+     * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
+     * aleatoriamente no intervalo de 0 a 50, em seguida, separe os números pares e os números impares do
+     * vetor gerador em dois vetores, um somente com os números pares encontrados no vetor gerado e
+     * outro somente com os números impares encontrados no vetor gerado.
+     *
+     * Em seguida, escrever o vetor gerado, o vetor somente de números pares e
+     * o vetor somente de números impares.
+     *
+     * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
+     * definidos em variáveis constantes.
+     */
+    void exercicio07(void);
 
-/**
- * Faça um algoritmo que gere um vetor de 5 posições com números primos preenchidos
- * aleatoriamente no intervalo de 0 a 100.
- *
- * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
- * definidos em variáveis constantes.
- */
-void exercicio09(void);
+    /**
+     * Faça um algoritmo que gere um vetor de 5 posições preenchido com números aleatoriamente
+     * gerados. Só serão admitidos no vetor números divisíveis por 3 no intervalo de 0 a 100.
+     *
+     * Escrever o vetor gerado.
+     *
+     * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
+     * definidos em variáveis constantes.
+     */
+    void exercicio08(void);
 
-/**
- * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
- * aleatoriamente no intervalo de 0 a 50. Os números não podem ser repetidos.
- *
- * Em seguida, escreva os números gerados.
- *
- * O tamanho do vetor e os limites inferior e superior de geração dos números
- * aleatórios deverão ser definidos em variáveis constantes.
- */
-void exercicio10(void);
+    /**
+     * Faça um algoritmo que gere um vetor de 5 posições com números primos preenchidos
+     * aleatoriamente no intervalo de 0 a 100.
+     *
+     * O tamanho do vetor e o limite de geração dos números aleatórios deverão ser
+     * definidos em variáveis constantes.
+     */
+    void exercicio09(void);
+
+    /**
+     * Faça um algoritmo que gere um vetor de 10 posições com números inteiros preenchidos
+     * aleatoriamente no intervalo de 0 a 50. Os números não podem ser repetidos.
+     *
+     * Em seguida, escreva os números gerados.
+     *
+     * O tamanho do vetor e os limites inferior e superior de geração dos números
+     * aleatórios deverão ser definidos em variáveis constantes.
+     */
+    void exercicio10(void);
+}
 
 #endif
