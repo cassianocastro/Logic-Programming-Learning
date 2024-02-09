dc7d92e90d13bc104cb5cd7ec1323587e153bac0
Cassiano Castro <development.castro@gmail.com>
2024-02-09 16:15:11 -0300

    Added exercises description

--- src/includes/List_6.hpp
+++ src/includes/List_6.hpp
@@ -6,52 +6,148 @@ namespace List_6
 {
 
     /**
+     * Faça um programa que leia uma matriz 4 x 4 com números inteiros,
+     * em seguida, escreva a matriz resultante em forma de tabela 4 x 4,
+     * conforme exemplo abaixo:
      *
+     * | 0 | 1 | 2 | 3 |
+     * |:---:|:---:|:---:|:---:|
+     * | 1 | 3 | 4 | 7 |
+     * | 5 | 2 | 9 | 8 |
+     * | 6 | 0 | 2 | 5 |
+     * | 7 | 6 | 2 | 1 |
      */
     void exercise01(void);
 
     /**
+     * Faça um programa que preencha uma matriz 5 x 5 com
+     * números inteiros aleatórios, no intervalo entre 0 e 100.
      *
+     * Em seguida, escreva a matriz resultante em forma de tabela 5 x 5.
      */
     void exercise02(void);
 
     /**
+     * Faça uma programa que preencha uma matriz 5 x 5 com números de 10 a 34,
+     * conforme exemplo abaixo:
      *
+     * | 0 | 1 | 2 | 3 | 4 |
+     * |:---:|:---:|:---:|:---:|:---:|
+     * | 10 | 11 | 12 | 13 | 14 |
+     * | 15 | 16 | 17 | 18 | 19 |
+     * | 20 | 21 | 22 | 23 | 24 |
+     * | 25 | 26 | 27 | 28 | 29 |
+     * | 30 | 31 | 32 | 33 | 34 |
      */
     void exercise03(void);
 
     /**
+     * Faça um programa que gere uma matriz 4 x 4 com números inteiros
+     * aleatórios entre 0 e 100, em seguida mostre:
      *
+     * - A soma dos valores digitados na matriz
+     * - A média dos números digitados na matriz
      */
     void exercise04(void);
 
     /**
+     * Faça um programa que gere uma matriz 4 x 4 com números inteiros
+     * aleatórios entre 0 e 100, em seguida mostre:
      *
+     * - O maior elemento da matriz e sua respectiva posição,
+     * ou seja, linha e coluna.
+     *
+     * - O menor elemento da matriz e sua respectiva posição,
+     * ou seja, linha e coluna.
+     *
+     * Caso o maior ou o menor número estiverem presentes repetidas vezes na
+     * matriz, o sistema deverá informar a posição do primeiro maior ou
+     * do primeiro menor encontrados.
      */
     void exercise05(void);
 
     /**
+     * Faça um programa que gere uma matriz 4 x 4 com números inteiros
+     * aleatórios entre 0 e 100, todavia, a matriz será preenchida seguindo
+     * a seguinte norma:
      *
+     * - A primeira e a terceira linha receberão somente números pares
+     *
+     * - A segunda e a quarta linhas receberão somente números impares
      */
     void exercise06(void);
 
     /**
+     * Faça um programa que carregue uma matriz 5 x 5 preenchida com números
+     * pares de 2 a 50 e escreva a matriz resultante(original).
      *
+     * Em seguida, divida cada número por 2 e armazene o resultado na mesma posição
+     * do número que foi dividido, escrevendo a matriz modificada(resultante).
+     *
+     * Matriz Original:
+     *
+     * | 0 | 1 | 2 | 3 | 4 |
+     * |:---:|:---:|:---:|:---:|:---:|
+     * | 2  | 4  | 6  | 8  | 10 |
+     * | 12 | 14 | 16 | 18 | 20 |
+     * | 22 | 24 | 26 | 28 | 30 |
+     * | 32 | 34 | 36 | 38 | 40 |
+     * | 42 | 44 | 46 | 48 | 50 |
+     *
+     * Matriz resultante:
+     *
+     * | 0 | 1 | 2 | 3 | 4 |
+     * |:---:|:---:|:---:|:---:|:---:|
+     * | 1  | 2  | 3  | 4  | 5 |
+     * | 6 | 7 | 8 | 9 | 10 |
+     * | 11 | 12 | 13 | 14 | 15 |
+     * | 16 | 17 | 18 | 19 | 20 |
+     * | 21 | 22 | 23 | 24 | 25 |
      */
     void exercise07(void);
 
     /**
+     * Faça um programa que carregue uma matriz 5 x 5 com números inteiros
+     * preenchidos de 1 a 25 e exiba a matriz gerada.
      *
+     * Em seguida, calcule e mostre a soma:
+     *
+     * - Dos elementos da linha 4
+     * - Dos elementos da coluna 2
+     * - Dos elementos da diagonal principal(representada em verde)
+     * - Dos elementos da diagonal secundária(representada em amarelo)
+     * - De todos os elementos da matriz
      */
     void exercise08(void);
 
     /**
+     * Faça um programa que carregue uma matriz 3 x 3 somente com números
+     * primos gerados de forma aleatória no intervalo de 0 à 100.
      *
+     * Escrever a matriz gerada.
      */
     void exercise09(void);
 
     /**
+     * Os proprietários de um teatro que possui 25 lugares necessitam de uma
+     * ferramenta de software para reservas de lugares.
      *
+     * O desenvolvedor contratado verificou que as poltronas disponíveis para
+     * reservas são referenciados pelo número da fila (a partir do número 0) e
+     * pelo número da cadeira (a partir do número 0) em cada fila,
+     * em uma representação matricial em que as linhas e colunas correspondem,
+     * respectivamente, às filas e às colunas de cadeiras.
+     *
+     * Implemente um programa que realize as reservas dos lugares (informando
+     * a fila e a cadeira), marcando com a letra “R” os lugares reservados e
+     * com “L” os lugares livres no teatro.
+     *
+     * A cada reserva, mostrar um “mapa” do teatro.
+     *
+     * Caso a cadeira já tenha sido reservada, exibir uma mensagem ao
+     * usuário e solicitar nova posição.
+     *
+     * Para encerrar as reservas, informar "s".
      */
     void exercise10(void);
 }
--- src/includes/List_7.hpp
+++ src/includes/List_7.hpp
@@ -6,52 +6,135 @@ namespace List_7
 {
 
     /**
+     * Faça um programa que carregue uma matriz 6 x 4 com números inteiros.
      *
+     * Recalcule a matriz digitada, onde cada linha será multiplicada pelo
+     * maior elemento da linha em questão.
+     *
+     * Mostre a matriz resultante.
+     *
+     * A matriz será carregada com números aleatórios de 0 à 100.
+     *
+     * O programa irá ser repetido até que seja digitado 's'.
      */
     void exercise01(void);
 
     /**
+     * Faça um programa que carregue uma matriz 5 x 5 com números inteiros
+     * randômicos de 0 à 100, em seguida, crie dois vetores de cinco posições
+     * cada um, que contenham, respectivamente, as somas das linhas e das
+     * colunas da matriz.
      *
+     * Escreva a matriz e os vetores criados.
      */
     void exercise02(void);
 
     /**
-     *
+     * Dada uma matriz de números inteiros de dimensão 5 x 5, preenchida de
+     * forma randômica de 0 à 100, verificar se existem elementos repetidos.
      */
     void exercise03(void);
 
     /**
+     * Dada uma matriz de números inteiros de dimensão 5 x 5, preenchida de
+     * forma randômica de 100 à 300, verificar se existem números que são
+     * divisíveis por 3.
      *
+     * Escrever os números encontrados.
      */
     void exercise04(void);
 
     /**
+     * Faça um programa que preencha um matriz 10 x 10 com números inteiros de
+     * forma randômica no intervalo 0 a 500.
      *
+     * Em seguida, o programa solicita que seja informado um número e faz a
+     * busca pelo número digitado na matriz.
+     *
+     * Se o número informado estiver presente na matriz, o programa exibe uma
+     * mensagem “Número encontrado” e descreve a quantidade de vezes que o
+     * número está repetido, caso contrário, exibe a mensagem “Número não localizado”.
+     *
+     * O programa deverá finalizar a busca se for digitado um número negativo.
      */
     void exercise05(void);
 
     /**
-     *
+     * Faça um programa que preencha um matriz 5 x 5 com números inteiros
+     * não repetidos de forma randômica no intervalo de 0 à 500 e escreva a
+     * matriz gerada.
      */
     void exercise06(void);
 
     /**
-     *
+     * Faça um programa que preencha um matriz 10 x 10 com números inteiros
+     * pares não repetidos de forma randômica no intervalo de 0 à 1000 e escreva
+     * a matriz gerada.
      */
     void exercise07(void);
 
     /**
+     * Faça um programa que preencha um matriz 5 x 5 preenchida de forma
+     * randômica com números de 0 à 100 da seguinte forma:
+     *
+     * x: números pares
+     * y: números primos
+     * z: números ímpares
+     *
      *
      */
     void exercise08(void);
 
     /**
-     *
+     * Faça um programa que preencha um matriz 5 x 5 de forma randômica com
+     * números de 0 a 100, em seguida, troque os elementos da 1º linha com os
+     * elementos da 1º coluna, os da 2º linha com a 2º coluna, os da 3º linha
+     * com a 3º coluna, os da 4º linha com a 4º coluna e os os da 5º linha com
+     * a 5º coluna.
      */
     void exercise09(void);
 
     /**
+     * Faça um programa que controle a pontuação de um campeonato de futebol.
+     * O número de equipes será definido por uma variável constante, no caso,
+     * 5 times disputam o campeonato.
      *
+     * Em um vetor de tamanho definido pela constante, (no caso, 5) serão
+     * armazenados os nomes dos times.
+     *
+     * Em uma matriz (definidos na constante, no caso, de dimensão 5x4) é
+     * armazenada a pontuação dos jogos (os times jogam entre sí).
+     *
+     * Cada linha da matriz corresponde a uma posição no vetor, o que faz com
+     * que a primeira linha da matriz contenha os pontos dos times da primeira
+     * posição do vetor, a segunda linha da matriz corresponde a segunda posição
+     * do vetor e assim por diante...
+     *
+     * O placar de cada jogo será definido de forma aleatória, com, no
+     * máximo, 4 gols por equipe.
+     *
+     * Vitória conta 3 pontos para o vencedor e 0 para o derrotado, empate 1 ponto
+     * para cada equipe.
+     *
+     * Ao final de inserção de dados, o programa deve calcular o número máximo de
+     * pontos de cada equipe. Exemplo:
+     *
+     * Vetor dos times
+     *
+     * | 0 | 1 | 2 | 3 | 4 |
+     * |:---:|:---:|:---:|:---:|:---:|
+     * | Inter | Grêmio | Santos | Vasco | Sport |
+     *
+     * Matriz dos jogos e pontos das equipes
+     *
+     * |         | Jogo 1 | Jogo 2 | Jogo 3 | Jogo 4 |
+     * |:---:    |:---:   |:---:   |:---:   |:---:   |
+     * | posição | 0      | 1      | 2      | 3      |
+     * | 0 | 3 | 3 | 1 | 1 |
+     * | 1 | 0 | 1 | 3 | 1 |
+     * | 2 | 0 | 1 | 3 | 1 |
+     * | 3 | 1 | 0 | 0 | 0 |
+     * | 4 | 1 | 1 | 1 | 3 |
      */
     void exercise10(void);
 }
