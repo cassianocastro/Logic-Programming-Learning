431deaea02b3d087304c6ba9fdc9b83f5480d9ad
Cassiano Castro <development.castro@gmail.com>
2024-02-09 16:30:34 -0300

    Added function definitions

--- src/app/List_6.cpp
+++ src/app/List_6.cpp
@@ -1,1 +1,51 @@
 #include "../includes/List_6.hpp"
+
+void List_6::exercise01(void)
+{
+
+}
+
+void List_6::exercise02(void)
+{
+
+}
+
+void List_6::exercise03(void)
+{
+
+}
+
+void List_6::exercise04(void)
+{
+
+}
+
+void List_6::exercise05(void)
+{
+
+}
+
+void List_6::exercise06(void)
+{
+
+}
+
+void List_6::exercise07(void)
+{
+
+}
+
+void List_6::exercise08(void)
+{
+
+}
+
+void List_6::exercise09(void)
+{
+
+}
+
+void List_6::exercise10(void)
+{
+
+}
--- src/app/List_7.cpp
+++ src/app/List_7.cpp
@@ -1,1 +1,51 @@
 #include "../includes/List_7.hpp"
+
+void List_7::exercise01(void)
+{
+
+}
+
+void List_7::exercise02(void)
+{
+
+}
+
+void List_7::exercise03(void)
+{
+
+}
+
+void List_7::exercise04(void)
+{
+
+}
+
+void List_7::exercise05(void)
+{
+
+}
+
+void List_7::exercise06(void)
+{
+
+}
+
+void List_7::exercise07(void)
+{
+
+}
+
+void List_7::exercise08(void)
+{
+
+}
+
+void List_7::exercise09(void)
+{
+
+}
+
+void List_7::exercise10(void)
+{
+
+}
